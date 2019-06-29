6e71166b666ef1ad93efb5ab1d7f1c86bd0e9f1c
Sarah-Shahinpour <slssarah101@gmail.com>
2019-06-29 18:54:14 -0400

    gets random images and displays text on top

--- src/main/webapp/js/feed.js
+++ src/main/webapp/js/feed.js
@@ -1,18 +1,6 @@
- // Fetch messages and add them to the page.
+ // Fetch messages and add them to the slideshow.
   function fetchMessages(){
     const url = '/feed';
-    //In this commented section I tried iterating through the message 
-    //objects differently where it would return an array of 
-    //the image URLs
-
-    // var response = fetch(url).json();
-    // //This will be a list of the message URLs to put in the slider
-    // var messageImages = [];
-    // for (var message in response){
-    //   messageImages.push(messageToImage(message));
-    // }
-    // return messageImages; 
-
     fetch(url).then((response) => {
       return response.json();
     }).then((messages) => {
@@ -27,40 +15,16 @@
       //This will be a list of the message URLs to put in the slider
       messages.forEach((message) => {
         imgs.push(messageToImage(message)); 
+        //This stores all the messages in an array
+        imgText.push(message.text);
       });
     });
   }
 
   //Takes in a message and returns the image URL
   function messageToImage(message){
-    //This is one method where it would create an image using canvas
-    // var canvas = document.createElement("CANVAS");
-    // canvas.fillstyle = "red";
-    // var dataURL = canvas.toDataURL("image/png");
-    // return dataURL;
-    
-    //The rest of this function grabs a random image from flickr we could
-    //possibly use as a background. I saw this code a few times when
-    //I was searching online
-    var keyword = "mountains";
-
-    $(document).ready(function(){
-
-        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-        {
-            tags: keyword,
-            tagmode: "any",
-            format: "json"
-        },
-        function(data) {
-            var rnd = Math.floor(Math.random() * data.items.length);
-
-            var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
-
-        });
-
-    });
-    return ('" + image_src + "');
+    //gets a random image 400x400
+    return ("http://lorempixel.com/400/400");
   }
 
   // Fetch data and populate the UI of the page.
@@ -70,6 +34,7 @@
 
 
   var imgs = [];
+  var imgText = [];
   fetchMessages();
   document.addEventListener("keydown", imgCycle, false);
 
@@ -84,6 +49,8 @@
   function changeImage(dir){
     var img = document.getElementById("imgClickAndChange");
     img.src = imgs[imgs.indexOf(img.src) + (dir || 1)] || imgs[dir ? imgs.length - 1 : 0];
+    var messageText = document.getElementById("messageText");
+    messageText.innerHTML = imgText[imgText.indexOf(img.src) + (dir || 1)] || imgs[dir ? imgs.length - 1 : 0];
   }
 
     
