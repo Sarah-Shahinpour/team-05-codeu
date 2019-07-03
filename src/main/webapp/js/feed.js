aca4bf4c84e73beedcb944019efd6cc9d68a9862
AhmedAlzubairi1 <aalzuba000@citymail.cuny.edu>
2019-07-03 00:17:59 -0400

    Added ability that key strokes would go in a circular faction,thus when pressing left at index 0, you will go to the last index and when pressing right on last index, you will go first index. Else, if pressing right, move up index, if pressing left, move down an index

--- src/main/webapp/js/feed.js
+++ src/main/webapp/js/feed.js
@@ -64,10 +64,33 @@
     messageText.innerHTML = imgText[0];
 
   }
+  var current=0;
   function changeImage(dir){
     var img = document.getElementById("imgClickAndChange");
-    img.src = imgs[imgs.indexOf(img.src) + (dir || 1)] || imgs[dir ? imgs.length - 1 : 0];
+    //img.src = imgs[imgs.indexOf(img.src) + (dir || 1)] || imgs[dir ? imgs.length - 1 : 0];
     var messageText = document.getElementById("messageText");
-    messageText.innerHTML = imgText[imgText.indexOf(messageText.innerHTML) + (dir || 1)] || imgText[dir ? imgText.length - 1 : 0];
- 
+    //messageText.innerHTML = imgText[imgText.indexOf(messageText.innerHTML) + (dir || 1)] || imgText[dir ? imgText.length - 1 : 0];
+    if(dir==1){
+      //Going Right
+      //0 1 2 3     Current ranges between it, length is 3
+      if(current==imgText.length-1){
+        current=0;
+      }
+      else{
+        current=current+1;
+
+      }
+
+    }
+    else{
+      //Going Left
+      if(current==0){
+        current=imgText.length-1;
+      }
+      else{
+        current=current-1;
+      } 
+    } 
+  img.src = imgs[current];
+  messageText.innerHTML = imgText[current];
   }
